749b7f6f6b96bae38fea6e593e6752414a59eb2f
Jielianglin <jielianglin821@gmail.com>
2022-06-27 15:23:09 +0200

    implemented links in navigation bar

--- kute-app/src/components/Nav-bar.js
+++ kute-app/src/components/Nav-bar.js
@@ -12,9 +12,20 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import Link from '@mui/material/Link';
+import "./Nav-bar.css";
 
-const pages = ['Friends', 'Events', 'Chat', 'Edit Profile'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+// const pages = [
+
+//     { name: 'Friends', location: "/friends" },
+//     { name: 'Events', location: "/events" },
+//     { name: 'Chat', location: "/chat" },
+//     { name: 'Profile', location: "/profile" }
+
+// ];
+
+
+
 
 const ResponsiveAppBar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -55,7 +66,6 @@ const ResponsiveAppBar = () => {
                             textDecoration: 'none',
                         }}
                     >
-                        [K]ute
                     </Typography>
 
                     {/* collapsed Nav */}
@@ -89,46 +99,48 @@ const ResponsiveAppBar = () => {
                                 display: { xs: 'block', md: 'none' },
                             }}
                         >
-                            {pages.map((page) => (
-                                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                    <Typography textAlign="center">{page}</Typography>
-                                </MenuItem>
-                            ))}
+
+                            <MenuItem onClick={handleCloseNavMenu}>
+                                <ul className="hamburgerLinks">
+                                    <li><Link href="/friends">Friends</Link></li>
+
+                                    <li><Link href="/events">Events</Link></li>
+
+                                    <li><Link href="/buddies">Buddies</Link></li>
+
+                                    <li><Link href="/chat">Chat</Link></li>
+                                </ul>
+                            </MenuItem>
+
                         </Menu>
                     </Box>
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'primary',
-                            textDecoration: 'none',
-                        }}
-                    >
 
-                    </Typography>
 
                     {/* Navbar Links */}
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'black', display: 'block' }}
-                            >
-                                {page}
-                            </Button>
-                        ))}
+                        <div className="link">
+                            <Link href="/friends" onClick={handleCloseNavMenu}>
+                                Friends
+                            </Link>
+                        </div>
+                        <div className="link">
+                            <Link href="/events" onClick={handleCloseNavMenu}>
+                                Events
+                            </Link>
+                        </div>
+                        <div className="link">
+                            <Link href="/chat" onClick={handleCloseNavMenu}>
+                                Chat
+                            </Link>
+                        </div>
+                        <div className="link">
+                            <Link href="/profile" onClick={handleCloseNavMenu}>
+                                Edit Profile
+                            </Link>
+                        </div>
                     </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
+                    {/* <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
@@ -155,11 +167,11 @@ const ResponsiveAppBar = () => {
                                     <Typography textAlign="center">{setting}</Typography>
                                 </MenuItem>
                             ))}
-                        </Menu>
-                    </Box>
+                        </Menu> */}
+                    {/* </Box> */}
                 </Toolbar>
             </Container>
-        </AppBar>
+        </AppBar >
     );
 };
 export default ResponsiveAppBar;
