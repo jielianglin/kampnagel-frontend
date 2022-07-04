376a6cc73e547369a2912d280abcb3a9207fd44e
Jielianglin <jielianglin821@gmail.com>
2022-07-04 12:53:09 +0200

    formatted chatbox

--- kute-app/src/components/chat/Chatbox.js
+++ kute-app/src/components/chat/Chatbox.js
@@ -1,26 +1,35 @@
 import React from "react";
 import "./Chatbox.css";
+import Chip from '@mui/material/Chip';
 
 export default function ChatBox() {
     return (
+
+
         <div className="chatbox">
 
-            <div className="message-container">
-                <div className="client">client id</div>
-                <div className="message">Hey</div>
-            </div>
+            <div className="messages">
+                <div className="messages-left">
 
-            <div className="another-message-container">
-                <div className="client">client id</div>
-                <div className="message">Hey</div>
-            </div>
+                    <div className="message"><Chip label="hey" /></div>
+                    <div className="timestamp">12:00</div>
 
+                </div>
+                < br />
+                <div className="messages-right">
+                    <div className="message"><Chip label="hey" variant="outlined" /></div>
+                    <div className="timestamp">12:05</div>
+                </div>
+                < br />
+            </div>
             <hr />
-            <div className="input-chat-container">
-                <input className="input-chat" type="text" placeholder="Chat message..." />
-                <button className="submit-chat"> Send </button>
+            <div className="chat-input-container">
+                <input className="chat-input" type="text" placeholder="Chat message..." />
+                <button className="submit-button"> Send </button>
             </div>
 
         </div>
+
+
     )
 }; 
