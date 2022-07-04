64cc3f2458f614722241e15de855933ece0c44c6
Jielianglin <jielianglin821@gmail.com>
2022-07-04 15:32:00 +0200

    centered buddies grid

--- kute-app/src/components/friends/Friends-grid.js
+++ kute-app/src/components/friends/Friends-grid.js
@@ -2,38 +2,41 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Card from "./Friends-card";
+import "./Friends-grid.css";
 
 
 
 export default function RowAndColumnSpacing() {
     return (
-        <Box sx={{ width: '70%' }}>
-            <Grid container rowSpacing={6} columnSpacing={{ xs: 6, sm: 8, md: 10 }}>
-                <Grid item xs={3}>
-                    <Card />
+        <div className="buddies-cards-grid">
+            <Box sx={{ width: '70%' }}>
+                <Grid container rowSpacing={6} columnSpacing={{ xs: 6, sm: 8, md: 10 }}>
+                    <Grid item xs={3}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Card />
+                    </Grid>
                 </Grid>
-                <Grid item xs={3}>
-                    <Card />
-                </Grid>
-                <Grid item xs={3}>
-                    <Card />
-                </Grid>
-                <Grid item xs={3}>
-                    <Card />
-                </Grid>
-                <Grid item xs={3}>
-                    <Card />
-                </Grid>
-                <Grid item xs={3}>
-                    <Card />
-                </Grid>
-                <Grid item xs={3}>
-                    <Card />
-                </Grid>
-                <Grid item xs={3}>
-                    <Card />
-                </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        </div>
     );
 }
