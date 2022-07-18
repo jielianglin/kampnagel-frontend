57500583e6d833bbb99df38ee0ec054fd0460492
Jielianglin <jielianglin821@gmail.com>
2022-07-18 12:40:28 +0200

    implemented friends expanded card and scroll group
    toggle and integration incomplete

--- kute-app/src/components/friends/Friends-grid.js
+++ kute-app/src/components/friends/Friends-grid.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-// import sampleAvatar from "../pictures/sampleAvatar.png";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Chips from "./Friends-chips";
+import Collapse from '@mui/material/Collapse';
+
 import "./Friends-grid.css";
+
+var index;
 
 const imgsLinks = [
     "https://loremflickr.com/320/240?random=1",
@@ -23,8 +27,12 @@ const imgsLinks = [
 ];
 
 export default function BuddiesProfileCard() {
+    const [expanded, showExpanded] = React.useState(false);
+    const [collapsed, showCollapsed] = React.useState(true);
 
-    const images = imgsLinks.map(link => {
+
+    const images = imgsLinks.map((link) => {
+
         return (
             <LazyLoadImage
                 alt="img"
@@ -41,36 +49,101 @@ export default function BuddiesProfileCard() {
     }
     );
 
+
+    const expandCard = () => {
+        console.log("clicked");
+        console.log(index);
+        showExpanded(!expanded);
+        showCollapsed(!collapsed);
+    }
+
     return (
         <div className="row">
             <Box sx={{ width: '100%' }}>
                 <Grid container rowSpacing={6} columnSpacing={{ xs: 6, sm: 8, md: 10 }}>
-                    {images.map(image =>
+                    {images.map((image, index) =>
+
                         <div>
+
                             <Grid item xs={3}>
-                                <div className="card">
-                                    <Typography variant="h5"> <div className="buddies-username">Name</div> </Typography>
 
-                                    <Typography variant="body2" color="text.secondary" className="buddies-gender"> <div>Gender</div> </Typography>
-                                    <br />
-                                    <div>{image}</div>
-                                    <br />
-                                    <Typography variant="body2" color="text.secondary">
-                                        <ul className="buddies-interests-lists">
-                                            <li>
-                                                Interest 1
-                                            </li>
+                                {collapsed &&
+                                    <div className="collapsed-card">
+                                        <Typography variant="h5"> <div className="buddies-username">Name</div> </Typography>
+                                        <Typography variant="body2" color="text.secondary" className="buddies-gender"> <div>Gender</div> </Typography>
+                                        <br />
+                                        <div className="avatar-container-collapsed" onClick={expandCard} key={index}>
+                                            {image}
+                                        </div>
+                                        <br />
+                                        <Typography variant="body2" color="text.secondary">
+                                            <ul className="buddies-interests-lists">
+                                                <li>
+                                                    Interest 1
+                                                </li>
 
-                                            <li>
-                                                Interest 2
-                                            </li>
+                                                <li>
+                                                    Interest 2
+                                                </li>
 
-                                            <li>
-                                                Interest 3
-                                            </li>
-                                        </ul>
-                                    </Typography>
-                                </div >
+                                                <li>
+                                                    Interest 3
+                                                </li>
+                                            </ul>
+                                        </Typography>
+                                    </div >
+                                }
+                                {expanded &&
+                                    <div className="expanded-card">
+                                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                            <Box>
+                                                <section>
+                                                    <div className="expanded-header">
+                                                        <div className="username">
+                                                            Paloma
+                                                        </div>
+                                                        <div className="gender">
+                                                            she/her
+                                                        </div>
+                                                        <div className="age">
+                                                            Age: 36
+                                                        </div>
+                                                    </div>
+                                                    <div className="avatar-container-expanded" onClick={expandCard} key={index}
+                                                    >
+                                                        {image}
+                                                    </div>
+                                                    <div className="intro"> <Typography>Tickle my curiosity!</Typography></div>
+                                                    <br />
+                                                    <div className="connect-button-container"> <button className="connect-button"> CONNECT</button></div>
+                                                </section>
+                                                <section className="expanded-scroll-group">
+
+                                                    <div className="friends-scroll-category">
+                                                        <Typography>INTERESTS</Typography>
+                                                    </div>
+                                                    <div>
+                                                        <Chips />
+                                                    </div>
+                                                    <br />
+                                                    <div className="friends-scroll-category">
+                                                        <Typography>EVENTS</Typography>
+                                                    </div>
+                                                    <div>
+                                                        <Chips />
+                                                    </div>
+                                                    <br />
+                                                    <div className="friends-scroll-category">
+                                                        <Typography>WHY I'M HERE</Typography>
+                                                    </div>
+                                                    <div>
+                                                        <Chips />
+                                                    </div>
+                                                </section>
+                                            </Box>
+                                        </Collapse>
+                                    </div>
+                                }
                             </Grid>
                         </div>
                     )}
